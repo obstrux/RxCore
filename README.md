e57a2cfa3c475772c6ece3168aa6e588bf252ca4
blades <1054050397@qq.com>
2016-10-14 17:30:20 +0800

    write the README.md

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # RxCore
+----
+
+project file build.gradle
+```
+	allprojects {
+		repositories {
+			...
+			maven { url "https://jitpack.io" }
+		}
+	}
+```
+
+module file build.gradle
+
+```
+    compile 'com.github.AlphaKnife:RxCore:v0.1'
+```
--- app/src/main/java/library/blades/rxcore/MainActivity.java
+++ app/src/main/java/library/blades/rxcore/MainActivity.java
@@ -1,13 +1,16 @@
 package library.blades.rxcore;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import library.rxlibrary.component.CoreRxActivity;
+
+public class MainActivity extends CoreRxActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
